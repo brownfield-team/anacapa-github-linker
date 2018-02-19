4a999c42d4ce67bf79d286c140d00891a78761d7
Daniel Shu <danielshu98@gmail.com>
2018-02-18 21:48:42 -0800

    Created two separate csv tests to test upload with and without header

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -17,7 +17,7 @@ class CourseTest < ActiveSupport::TestCase
     sign_in @user
   end
 
-  test "calling import_students should import students from csv" do
+  test "calling import_students should import students from csv WITHOUT header" do
 
     csv_file = fixture_file_upload('files/students.csv')
 
@@ -28,10 +28,21 @@ class CourseTest < ActiveSupport::TestCase
 
   end
 
+  test "calling import_students should import students from csv WITH header" do
+
+    csv_file = fixture_file_upload('files/students.csv')
+
+    csv_header_map = "student_id,email,first_name,last_name"
+    assert_difference('@course.roster_students.count', 3) do
+      @course.import_students(csv_file,csv_header_map,true)
+    end
+
+  end
+
   test "download to csv" do
     csv = @course.export_students_to_csv
 
-    #NOTE: The roster_students do not yet have a github username but the exported csv provides a column for itC
+    #NOTE: The roster_students do not yet have a github username but the exported csv provides a column for it
     expected_csv = "perm,email,first_name,last_name,github_username\n12345678,wes@email.com,Wes,P,\n21345678,tim@email.com,Tim,H,\n"
     assert_equal csv, expected_csv
   end
