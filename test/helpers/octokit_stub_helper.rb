10201f8c43e22e76fe9a68b063e98d94aa4c778e
Faiz Surani <faiz.surani@gmail.com>
2019-11-04 16:03:08 -0800

    Attempt to fix TravisCI test failure

--- test/helpers/octokit_stub_helper.rb
+++ test/helpers/octokit_stub_helper.rb
@@ -149,7 +149,6 @@ module OctokitStubHelper
       with(  headers: {
         'Accept'=>'application/vnd.github.v3+json',
         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'Authorization'=>'token a3a3b41ce3acedb58eb70c044b9dd5189896122c',
         'Content-Type'=>'application/json',
         'User-Agent'=>'Octokit Ruby Gem 4.8.0'
       }).
