5d00574cf7fa236b1d7cff5d98add4744679ae1a
Faiz Surani <faiz.surani@gmail.com>
2019-11-19 19:26:55 -0800

    Test job stores at most 10 records of completed_jobs per course

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -4,5 +4,8 @@ class TestJob
   def perform(url_triggered_from, course_id)
     CompletedJob.create(job_name: "Test Job", url_triggered_from: url_triggered_from,
                         course_id: course_id)
+    if CompletedJob.where('course_id = ' + course_id.to_s).count > 10
+      CompletedJob.where('course_id = ' + course_id.to_s).order(:created_at).first.destroy
+    end
   end
 end
