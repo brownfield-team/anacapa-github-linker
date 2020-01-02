ddbd247d0979d44de6daa7efaef9cc58867d5150
Faiz Surani <faiz.surani@gmail.com>
2020-01-02 14:51:03 -0800

    Consolidated create and rename column migrations

--- db/migrate/20191217021033_add_short_name_to_completed_jobs.rb
+++ db/migrate/20191217021033_add_short_name_to_completed_jobs.rb
@@ -1,5 +1,5 @@
 class AddShortNameToCompletedJobs < ActiveRecord::Migration[5.1]
   def change
-    add_column :completed_jobs, :short_name, :string
+    add_column :completed_jobs, :job_short_name, :string
   end
 end
