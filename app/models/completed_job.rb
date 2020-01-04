8b2ae137523ce992309608663971f72459f355bc
Faiz Surani <faiz.surani@gmail.com>
2020-01-03 20:03:21 -0800

    Added time elapsed to job log

--- app/models/completed_job.rb
+++ app/models/completed_job.rb
@@ -1,5 +1,14 @@
 # CompletedJob is no longer an accurate name for this model as the table now stores in-progress jobs as well.
 # It has not been renamed yet to avoid associated headaches.
 
+# Adds distance_of_time_in_words method
+require 'action_view'
+include ActionView::Helpers::DateHelper
+
 class CompletedJob < ApplicationRecord
+  def time_elapsed
+    return distance_of_time_in_words(created_at, DateTime.now, include_seconds: true) if summary == "In progress"
+    distance_of_time_in_words(created_at, updated_at, include_seconds: true)
+  end
+
 end
