5c554e9a47e75a8a2309823239edd146e0ee7324
Faiz Surani <faiz.surani@gmail.com>
2020-01-04 16:25:59 -0800

    Wrote repo collaborators job and made necessary structural changes
    Including creating a join table model called RepoContributor to allow for how matched columns and dropping the old join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191217022023) do
+ActiveRecord::Schema.define(version: 20200104214922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,12 +41,17 @@ ActiveRecord::Schema.define(version: 20191217022023) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "repo_id"
+    t.string "full_name"
     t.index ["course_id"], name: "index_github_repos_on_course_id"
   end
 
-  create_table "github_repos_users", id: false, force: :cascade do |t|
-    t.bigint "github_repo_id", null: false
-    t.bigint "user_id", null: false
+  create_table "repo_contributors", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "github_repo_id"
+    t.boolean "substring_matched"
+    t.boolean "api_matched"
+    t.index ["github_repo_id"], name: "index_repo_contributors_on_github_repo_id"
+    t.index ["user_id"], name: "index_repo_contributors_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
