796700394f9f3466dee9ea13e37a2a27018660ce
Faiz Surani <faiz.surani@gmail.com>
2020-01-07 22:40:20 -0800

    Fixed raw SQL query in find_contributors

--- app/controllers/courses/roster_students_controller.rb
+++ app/controllers/courses/roster_students_controller.rb
@@ -92,12 +92,12 @@ module Courses
     helper_method :find_org_repos
 
     def find_contributors(repo_full_name)
-      # This query gets all the repositories a user contributes to and how the repo was matched to the user.
+      # This query gets certain information about a student, their user, and relationship to the repository in question.
       # It is written in raw SQL because it would take several queries using Rails syntax.
       query = <<-SQL
         SELECT rs.first_name, rs.last_name, rs.id, u.username, rc.permission_level
         FROM users u
-          JOIN roster_students rs ON (u.id = rs.user_id and 1 = rs.course_id)
+          JOIN roster_students rs ON (u.id = rs.user_id and #{@parent.id} = rs.course_id)
           JOIN repo_contributors rc ON u.id = rc.user_id
       SQL
       ActiveRecord::Base.connection.exec_query(query)
