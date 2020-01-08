b6e2cefdccfe3c8db3e06fb1117bebf1e9de4815
Faiz Surani <faiz.surani@gmail.com>
2020-01-08 15:42:46 -0800

    Added filtering to users list page

--- app/jobs/purge_unused_users_job.rb
+++ app/jobs/purge_unused_users_job.rb
@@ -1,4 +1,5 @@
 class PurgeUnusedUsersJob < AdminJob
+
   @job_name = "Purge Unenrolled Student Users"
   @job_short_name = "purge_unenrolled_users"
   @confirmation_dialog = "Are you sure you want to delete all non-admin/instructor users not enrolled in a course?"
@@ -10,7 +11,7 @@ class PurgeUnusedUsersJob < AdminJob
       users = User.all
       users.each do |user|
         if user.roster_students.size == 0
-          unless instructor_or_admin?(user)
+          unless UsersHelper.instructor_or_admin?(user)
             user.destroy
             num_removed += 1
           end
@@ -19,14 +20,6 @@ class PurgeUnusedUsersJob < AdminJob
       summary = num_removed.to_s + " users purged from the database."
       update_job_record_with_completion_summary(summary)
     end
-  end
-
-  # Slightly hacky way to tell whether user is an instructor as the user.has_role function
-  # requires a specific course to correctly tell whether a user has an instructor role.
-  # We want to know if the user is an instructor for ANY course.
-  def instructor_or_admin?(user)
-    user.roles.each { |role| return true if role.name == "admin" || role.name == "instructor" }
-    false
   end
 
 end
