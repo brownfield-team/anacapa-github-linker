e8423d988e5ce08e1c1a4a03cbc8879fc2a21d57
Faiz Surani <faiz.surani@gmail.com>
2020-03-13 16:26:12 -0700

    Superficially removed outward facing references to perm with student ID

--- app/models/roster_student.rb
+++ app/models/roster_student.rb
@@ -1,8 +1,8 @@
 class RosterStudent < ApplicationRecord
   belongs_to :course, optional: true
   belongs_to :user, optional: true 
-  validates :perm, presence: true, uniqueness: {scope: :course, message: "only unique perms in a class"}
-  validates :email, presence: true, uniqueness: {scope: :course, message: "only unique emails in a class", case_sensitive: false }
+  validates :perm, presence: true, uniqueness: {scope: :course, message: "Student IDs in a class must be unique."}
+  validates :email, presence: true, uniqueness: {scope: :course, message: "Student emails in a class must be unique.", case_sensitive: false }
   def username 
     # TODO: when linking roster_students to user accounts is implemented, this should return the name 
     # of this roster student's associated user
