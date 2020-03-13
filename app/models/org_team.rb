b20a6105b6fdfeed4ca21c85f35166e692db3eb5
Faiz Surani <faiz.surani@gmail.com>
2020-03-13 14:56:38 -0700

    Worked on teams view and fixed bug in team collaborator processing

--- app/models/org_team.rb
+++ app/models/org_team.rb
@@ -2,4 +2,6 @@ class OrgTeam < ApplicationRecord
   belongs_to :course
   has_many :student_team_memberships
   has_many :roster_students, through: :student_team_memberships
+  has_many :repo_team_contributors
+  has_many :github_repos, through: :repo_team_contributors
 end
