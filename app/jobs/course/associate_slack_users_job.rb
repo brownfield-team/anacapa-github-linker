3f57d6b485a5fcec437ac3d54e036870318eee2c
Faiz Surani <faiz.surani@gmail.com>
2020-03-27 15:16:06 -0700

    Slack association job is done in principle

--- app/jobs/course/associate_slack_users_job.rb
+++ app/jobs/course/associate_slack_users_job.rb
@@ -4,7 +4,7 @@ class AssociateSlackUsersJob < CourseJob
   @job_description = "Fetches a list of users in the linked Slack workspace and associates them by email with students in the course."
 
   def attempt_job
-    # Remember to check that id is not USLACKBOT
+    # TODO: Enable pagination of users list
     users_response = slack_machine_user.users_list
     unless users_response.respond_to? :ok && users_response.ok
       puts users_response
@@ -12,10 +12,25 @@ class AssociateSlackUsersJob < CourseJob
       return
     end
     slack_members = users_response.members
+    associations_created = 0; associations_updated = 0
     slack_members.each do |member|
-      if member.respond_to?(:profile) && member.profile.respond_to?(:email)
-
+      if member.profile.respond_to? :email
+        slack_user_record = SlackUser.find_by_uid(member.id)
+        if slack_user_record.nil?
+          email = member.profile.email.include? "@ucsb.edu" ? member.profile.email.sub("@ucsb.edu", "@umail.ucsb.edu") : member.profile.email
+          corresponding_student = RosterStudent.where(course_id: @course.id, email: email).first
+          if corresponding_student.nil? then next end
+          associations_created += 1
+          slack_user_record = SlackUser.new(uid: member.id, roster_student_id: corresponding_student.id)
+        else
+          associations_updated += 1
+        end
+        slack_user_record.username = member.name
+        slack_user_record.display_name = member.profile.display_name
+        slack_user_record.save
       end
     end
+    summary = "#{pluralize associations_created, "association"} created between students and Slack users, #{associations_updated} refreshed."
+    update_job_record_with_completion_summary(summary)
   end
 end
