6273f0d343022bf6e3a3099f65eb0c83039d7828
Faiz Surani <faiz.surani@gmail.com>
2020-04-05 11:36:55 -0700

    Added webhook action skeletons

--- app/controllers/courses/github_webhooks_controller.rb
+++ app/controllers/courses/github_webhooks_controller.rb
@@ -5,8 +5,60 @@ module Courses
     skip_before_action :verify_authenticity_token
     skip_before_action :authenticate_user!
 
-    def github_team(payload)
+    def github_organization(payload)
+      case payload[:action]
+      when "member_invited"
+      when "member_added"
+      when "member_removed"
+      when "renamed"
+      when "deleted"
+      else
+        return
+      end
+    end
 
+    def github_repository(payload)
+      case payload[:action]
+      when "created"
+      when "deleted"
+      when "edited"
+      when "renamed"
+      when "publicized"
+      when "privatized"
+      else
+        return
+      end
+    end
+
+    def github_member(payload)
+      case payload[:action]
+      when "added"
+      when "edited"
+      when "removed"
+      else
+        return
+      end
+    end
+
+    def github_team(payload)
+      case payload[:action]
+      when "created"
+      when "deleted"
+      when "edited"
+      when "added_to_repository"
+      when "removed_from_repository"
+      else
+        return
+      end
+    end
+
+    def github_membership(payload)
+      case payload[:action]
+      when "added"
+      when "removed"
+      else
+        return
+      end
     end
 
     def webhook_secret(payload)
