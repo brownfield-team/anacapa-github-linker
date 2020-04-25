5ed28b65dc3f1081c6b9cfd0c06bf3c0aa6540fe
Faiz Surani <faiz.surani@gmail.com>
2020-04-25 15:07:54 -0700

    Working react component

---
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default class HelloWorld extends React.Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+  };
+
+  /**
+   * @param props - Comes from your rails view.
+   */
+  constructor(props) {
+    super(props);
+
+    // How to set initial state in ES6 class syntax
+    // https://reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-class
+    this.state = { name: this.props.name };
+  }
+
+  updateName = (name) => {
+    this.setState({ name });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>
+          Hello, {this.state.name}!
+        </h3>
+        <hr />
+        <form >
+          <p>Also just want to look at the {this.props.course_organization}</p>
+          <label htmlFor="name">
+            Say hello to:
+          </label>
+          <input
+            id="name"
+            type="text"
+            value={this.state.name}
+            onChange={(e) => this.updateName(e.target.value)}
+          />
+        </form>
+      </div>
+    );
+  }
+}
