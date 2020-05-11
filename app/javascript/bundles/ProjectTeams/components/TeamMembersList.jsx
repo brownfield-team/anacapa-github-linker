71f79b4e668357f5cddb8c40b2f055d166b236ac
Faiz Surani <faiz.surani@gmail.com>
2020-05-11 16:35:24 -0700

    More team component work

--- app/javascript/bundles/ProjectTeams/components/TeamMembersList.jsx
+++ app/javascript/bundles/ProjectTeams/components/TeamMembersList.jsx
@@ -1,11 +1,12 @@
 import React, {Component, Fragment} from 'react';
 import * as PropTypes from 'prop-types';
 import {Badge} from "react-bootstrap";
+import {studentRoute} from "../services/teams-service-routes";
 
 class TeamMembersList extends Component {
     renderMember(member) {
         const student = member.roster_student;
-        const studentLink = `/courses/${student.course_id}/roster_students/${student.id}`
+        const studentLink = studentRoute(student.course_id, student.id);
         return (
           <Fragment key={member.id}>
               <a href={studentLink}><Badge pill variant="primary">{student.full_name}</Badge></a>
--- app/javascript/bundles/ProjectTeams/services/teams-service-routes.js
+++ app/javascript/bundles/ProjectTeams/services/teams-service-routes.js
@@ -2,5 +2,6 @@ const courseRoute = (courseId) => `/courses/${courseId}`;
 const courseProjectTeamsRoute = (courseId) => `${courseRoute(courseId)}/project_teams`
 const courseOrgTeamsRoute = (courseId) => `${courseRoute(courseId)}/org_teams`
 
+export const studentRoute = (courseId, studentId) => `${courseRoute(courseId)}/roster_students/${studentId}`
 export const projectTeamsRoute = (courseId) => `${courseProjectTeamsRoute(courseId)}.json`
 export const unaddedTeamsRoute = (courseId) => `${courseOrgTeamsRoute(courseId)}/unadded.json`;
