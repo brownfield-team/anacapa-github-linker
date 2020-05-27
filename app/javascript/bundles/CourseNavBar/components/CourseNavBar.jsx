dcbb0ef06e19a4976d0d7d10bd1f01630b7e18b3
Faiz Surani <faiz.surani@gmail.com>
2020-05-27 00:14:55 -0700

    Began work on course nav tabs and refactored course layout

---
+++ app/javascript/bundles/CourseNavBar/components/CourseNavBar.jsx
@@ -0,0 +1,18 @@
+import React, {Component, Fragment} from 'react';
+import * as PropTypes from 'prop-types';
+
+class CourseNavBar extends Component {
+    render() {
+        return (
+            <Fragment>
+
+            </Fragment>
+        );
+    }
+}
+
+CourseNavBar.propTypes = {
+    currentPath: PropTypes.string.isRequired
+};
+
+export default CourseNavBar;
