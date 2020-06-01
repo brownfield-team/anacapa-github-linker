3595f73cf571b6c4633318b47e1b1553749eb2e5
Faiz Surani <faiz.surani@gmail.com>
2020-06-01 01:01:59 -0700

    Attempted another fix on files changed logic

---
+++ app/javascript/components/ActivityDashboard/Student/StudentActivity.jsx
@@ -0,0 +1,16 @@
+import React, {Component, Fragment} from 'react';
+import * as PropTypes from 'prop-types';
+
+class StudentActivity extends Component {
+    render() {
+        return (
+            <Fragment>
+
+            </Fragment>
+        );
+    }
+}
+
+StudentActivity.propTypes = {};
+
+export default StudentActivity;
