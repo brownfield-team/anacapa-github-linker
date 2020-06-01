50df7a8d478d4d12e3d1e00e1590549277f1f91b
Faiz Surani <faiz.surani@gmail.com>
2020-06-01 11:06:26 -0700

    Added navigation buttons for create repos and create teams

--- app/javascript/bundles/CourseNavBar/components/CourseNavBar.jsx
+++ app/javascript/bundles/CourseNavBar/components/CourseNavBar.jsx
@@ -23,6 +23,8 @@ class CourseNavBar extends Component {
                     <NavDropdown title="Teams">
                         <MenuItem eventKey="project_teams" href={this.props.project_teams_path}>Project Teams</MenuItem>
                         <MenuItem eventKey="org_teams" href={this.props.org_teams_path}>GitHub Teams</MenuItem>
+                        <MenuItem eventKey="create_teams" href={this.props.create_teams_path}>Create Teams From CSV</MenuItem>
+                        <MenuItem eventKey="create_team_repos" href={this.props.create_team_repos_path}>Create Team Repos</MenuItem>
                     </NavDropdown>
                     <NavItem eventKey="repos" href={this.props.repos_path}>Repositories</NavItem>
                     <NavItem eventKey="slack" href={this.props.slack_path}>Slack</NavItem>
@@ -43,6 +45,8 @@ CourseNavBar.propTypes = {
     slack_path: PropTypes.string.isRequired,
     jobs_path: PropTypes.string.isRequired,
     edit_path: PropTypes.string.isRequired,
+    create_team_repos_path: PropTypes.string.isRequired,
+    create_teams_path: PropTypes.string.isRequired,
     can_edit: PropTypes.bool.isRequired
 };
 
