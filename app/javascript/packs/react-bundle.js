397796876b26241f80b577f294c862aa9a5ebd7f
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-07-19 19:18:10 -0700

    pc - rebase to pull in changes from #273 which added course github_repo index and show pages

--- app/javascript/packs/react-bundle.js
+++ app/javascript/packs/react-bundle.js
@@ -4,9 +4,12 @@ import Users from '../components/Users/Users';
 import ProjectTeams from "../components/ProjectTeams/ProjectTeams";
 import CourseNavBar from "../components/CourseNavBar/CourseNavBar";
 import StudentActivity from "../components/ActivityDashboard/Student/StudentActivity";
+
 import CourseGithubReposIndex from "../components/Course/GithubRepos/CourseGithubReposIndex";
 import CourseGithubRepo from "../components/Course/GithubRepos/CourseGithubRepo";
 
+import JobLauncher from "../components/Jobs/JobLauncher";
+import JobLog from "../components/Jobs/JobLog";
 
 import "../styles.css"
 
@@ -16,5 +19,7 @@ ReactOnRails.register({
   CourseNavBar,
   StudentActivity,
   CourseGithubReposIndex,
-  CourseGithubRepo
+  CourseGithubRepo,
+  JobLauncher,
+  JobLog
 });
