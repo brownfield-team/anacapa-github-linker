3c0467495a9c2b12b3799d7fec784760960dfdfb
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-07-19 17:41:13 -0700

    pc - refactor course github repos page so that controls work
    The ability to search repos now works
    The ability to select all, private, or public repos now works.

--- app/javascript/components/Course/GithubRepos/CourseGithubReposIndex.jsx
+++ app/javascript/components/Course/GithubRepos/CourseGithubReposIndex.jsx
@@ -1,21 +1,19 @@
 import React, { Component, Fragment } from 'react';
-import * as PropTypes from 'prop-types';
-import { Table } from "react-bootstrap";
+import PropTypes from 'prop-types';
 import CourseGithubReposTable from "./CourseGithubReposTable";
 import CourseGithubReposControls from "./CourseGithubReposControls";
 
 import axios from "../../../helpers/axios-rails";
-import ReposService from "../../../services/repos-service";
+import { Alert, Form } from 'react-bootstrap';
+import {debounce} from "debounce";
+
+import { githubReposRoute } from "../../../services/service-routes";
 
 class CourseGithubReposIndex extends Component {
-
     constructor(props) {
         super(props);
-        const csrfToken = ReactOnRails.authenticityToken();
-        axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
-        axios.defaults.params = {}
-        axios.defaults.params['authenticity_token'] = csrfToken;
-        this.state = { repos: [] };
+        this.state = { search: this.props.search ?? "", type: this.props.visibility ?? "", error: "", repos: [], page: 1, pageSize: 25, totalSize: 0 };
+        this.onSearchChanged = debounce(this.onSearchChanged, 1000);
     }
 
     componentDidMount() {
@@ -23,31 +21,92 @@ class CourseGithubReposIndex extends Component {
         this.updateRepos();
     }
 
+    onSearchChanged = (searchValue) => {
+        if (searchValue === this.props.search) {
+            return;
+        }
+        this.setState({search: searchValue}, () => {
+            this.updateRepos();
+        });
+    }
+
+    onVisibilityChanged = (visibilityValue) => {
+        if (visibilityValue === this.props.visibility) {
+            return;
+        }
+        this.setState({visibility: visibilityValue}, () => {
+           this.updateRepos();
+        });
+    }
+
+    paginationHandler = (page, pageSize) => {
+        this.setState({page: page, pageSize: pageSize}, () => {
+            this.updateRepos();
+        });
+    }
+
     updateRepos = () => {
         console.log("CourseGithubReposIndex updateRepos called");
         console.log("this.props=" + JSON.stringify(this.props));
 
-        ReposService.getGithubRepos(this.props.course_id).then(reposResponse => {
-            console.log("updateRepos setting state");
-            this.setState({ repos: reposResponse });
+        const url = githubReposRoute(this.props.course_id);
+        const params = {search: this.state.search, visibility: this.state.visibility, page: this.state.page, per_page: this.state.pageSize};
+        // Otherwise, calling setState fails because the scope for "this" is the success/error function.
+        const self = this;
+        Rails.ajax({
+            url: url,
+            type: "get",
+            data: $.param(params),
+            beforeSend: function() {
+                return true;
+            },
+            success: function (data, status, xhr) {
+                const totalRecords = parseInt(xhr.getResponseHeader("X-Total"));
+                const page = parseInt(xhr.getResponseHeader("X-Page"));
+                self.setState({ repos: data, totalSize: totalRecords, page: page, error: "" });
+            },
+            error: function (data) {
+                self.setState({ error: data });
+            }
         });
-    };
+    }
+
+    renderError() { // Or don't
+        const error = this.state.error;
+        return (
+            <div>
+            { error !== "" &&
+                <Alert id="error-alert" variant="danger"> {error} </Alert>
+            }
+            </div>
+        );
+    }
 
     render() {
         return (
-            <Fragment>
+            <div>
+                { this.renderError() }
                 <CourseGithubReposControls 
-                onSearchChanged={ ()=>{} }
-                onVisibilityChanged={ ()=>{}}
+                    onSearchChanged={this.onSearchChanged}
+                    onVisibilityChanged={this.onVisibilityChanged}
                 />
-                <CourseGithubReposTable repos={this.state.repos} {...this.props} />
-            </Fragment>
+                <CourseGithubReposTable
+                    repos={this.state.repos}
+                    page={this.state.page}
+                    pageSize={this.state.pageSize}
+                    totalSize={this.state.totalSize}
+                    paginationHandler={this.paginationHandler}
+                    {...this.props}
+                />
+            </div>
         );
     }
 }
 
 CourseGithubReposIndex.propTypes = {
-
+    search: PropTypes.string,
+    type: PropTypes.string,
+    course_id : PropTypes.string.isRequired
 };
 
 export default CourseGithubReposIndex;
