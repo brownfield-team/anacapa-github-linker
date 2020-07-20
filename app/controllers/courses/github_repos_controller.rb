4347b24e817a89f5ac5b81bf929d3674f93233dc
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-07-20 08:58:40 -0700

    pc - add a test job at the github_repo level
    This is scaffolding for adding a job that will get commits
    for a github_repo and store them in the database, as a way of
    getting commits that may have been missed by hook events, or
    after the fact.

--- app/controllers/courses/github_repos_controller.rb
+++ app/controllers/courses/github_repos_controller.rb
@@ -29,17 +29,6 @@ module Courses
       }
     end
 
-    def run_job
-      job_name = params[:job_name]
-      job = course_github_repo_job_list.find { |job| job.job_short_name == job_name }
-      # This is a hack. It should be replaced as soon as possible, hopefully after authorization in the app is redone.
-      if job.permission_level == "admin" && !user.has_role?("admin")
-        redirect_to course_jobs_path, alert: "You do not have permission to run this job. Ask an admin to run it for you."
-      end
-      job.perform_async(params[:course_id].to_i, params[:github_repo_id].to_i)
-      redirect_to course_github_repo_path(params[:course_id],params[:github_repo_id]), notice: "Job successfully queued."
-    end
-
     # List of course jobs to make available to run
     def course_github_repo_job_list
       # @course = Course.find(params[:course_id])
@@ -64,6 +53,18 @@ module Courses
     end
     helper_method :course_github_repo_job_info_list
 
+    def run_job
+      job_name = params[:job_name]
+      job = course_github_repo_job_list.find { |job| job.job_short_name == job_name }
+      # This is a hack. It should be replaced as soon as possible, hopefully after authorization in the app is redone.
+      if job.permission_level == "admin" && !user.has_role?("admin")
+        redirect_to course_jobs_path, alert: "You do not have permission to run this job. Ask an admin to run it for you."
+      end
+      job.perform_async(params[:course_id].to_i, params[:github_repo_id].to_i)
+      redirect_to course_github_repo_path(params[:course_id],params[:github_repo_id]), notice: "Job successfully queued."
+    end
+
+  
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_github_repo
