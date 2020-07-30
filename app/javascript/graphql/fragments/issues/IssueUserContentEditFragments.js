66a47ab7302c8272c0be8c984a481bc230b1c87e
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-07-30 13:36:36 -0700

    pc - factor out scaffold for two different issue stats components
    one for issue user content edits
    another for issue timeline event items

--- app/javascript/graphql/fragments/issues/IssueUserContentEditFragments.js
+++ app/javascript/graphql/fragments/issues/IssueUserContentEditFragments.js
@@ -6,9 +6,6 @@ export default class IssueUserContentEditFragments {
     static all() {
         return `
            ${this.userContentEditFields()}
-           ${this.issueTimelineItemsFields()}
-           ${this.addedToProjectEventFields()}
-           ${this.movedColumnsInProjectEventFields()}
         `
     }
    
@@ -30,48 +27,5 @@ export default class IssueUserContentEditFragments {
        `
      }
    
-    static issueTimelineItemsFields() {
-       return `
-         fragment issueTimelineItemsFields on IssueTimelineItems {
-           __typename
-           ... on AddedToProjectEvent { ... addedToProjectEventFields }
-           ... on MovedColumnsInProjectEvent { ... movedColumnsInProjectEventFields }
-         }
-       `;
-     }
-   
-    static addedToProjectEventFields() {
-       return `
-         fragment addedToProjectEventFields on AddedToProjectEvent {
-           id
-           actor {
-             ...actorFields
-           }
-           createdAt
-           project {
-             name
-             url
-           }
-           projectColumnName
-         }
-       `
-     }
-   
-    static movedColumnsInProjectEventFields() {
-       return `
-         fragment movedColumnsInProjectEventFields on MovedColumnsInProjectEvent {
-           id
-           actor {
-             ...actorFields
-           }
-           createdAt
-           project {
-             name
-             url
-           }
-           previousProjectColumnName
-           projectColumnName
-         }
-       `
-     }
+    
 }
