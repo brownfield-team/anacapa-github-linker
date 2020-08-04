377a33bd16f7b9595298b1ce7477681f7f6141c8
ahsun-tariq <33199088+ahsun-tariq@users.noreply.github.com>
2020-08-04 22:45:37 +0500

    at/pc added more timeline events

--- app/javascript/graphql/fragments/issues/IssueTimelineItemsFragments.js
+++ app/javascript/graphql/fragments/issues/IssueTimelineItemsFragments.js
@@ -6,7 +6,17 @@ export default class IssueTimelineItemsFragments {
            ${this.addedToProjectEventFields()}
            ${this.assignedEventFields()}
            ${this.closedEventFields()}
+           ${this.commentDeletedEventFields()}
+           ${this.connectedEventFields()}
+           ${this.convertedNoteToIssueEventFields()}
+           ${this.crossReferencedEventFields()}
+           ${this.demilestonedEventFields()}
+           ${this.disconnectedEventFields()}
+           ${this.issueCommentEventFields()}
+           ${this.labeledEventFields()}
+           ${this.milestonedEventFields()}
            ${this.movedColumnsInProjectEventFields()}
+           
         `
     }
 
@@ -17,7 +27,17 @@ export default class IssueTimelineItemsFragments {
            ... on AddedToProjectEvent { ... addedToProjectEventFields }
            ... on AssignedEvent {... assignedEventFields}
            ... on ClosedEvent {... closedEventFields}
+           ... on Comment {... issueCommentEventFields}
+           ... on ConnectedEvent {... connectedEventFields}
+           ... on CommentDeletedEvent {... commentDeletedEventFields}
+           ... on ConvertedNoteToIssueEvent {... convertedNoteToIssueEventFields}
+           ... on CrossReferencedEvent {... crossReferencedEventFields}
+           ... on DemilestonedEvent {... demilestonedEventFields }
+           ... on DisconnectedEvent {... disconnectedEventFields }
+           ... on LabeledEvent {... labeledEventFields }
+           ... on MilestonedEvent {... milestonedEventFields}
            ... on MovedColumnsInProjectEvent { ... movedColumnsInProjectEventFields }
+          
          }
        `;
      }
@@ -56,6 +76,7 @@ export default class IssueTimelineItemsFragments {
       `
     }
 
+
      static closedEventFields() {
       return `
         fragment closedEventFields on ClosedEvent {
@@ -67,8 +88,155 @@ export default class IssueTimelineItemsFragments {
         }
       `
     }
-  
-   
+
+    
+    static connectedEventFields() {
+      return `
+      fragment connectedEventFields on ConnectedEvent {
+        id
+        actor {
+          ...actorFields
+        }
+        createdAt
+        source {
+          ... on Issue {
+            number
+          }
+        }
+        subject {
+          ... on PullRequest {
+            number
+          }
+        }
+      }
+    `
+    }
+
+
+    static crossReferencedEventFields() {
+      return `
+        fragment crossReferencedEventFields on CrossReferencedEvent {
+          id
+          actor {
+            ...actorFields
+          }
+          createdAt
+          target {
+            ... on Issue {
+              number
+            }
+            ... on PullRequest {
+              number
+            }
+          }
+        }
+      `
+    }
+    
+    static convertedNoteToIssueEventFields() {
+      return `
+        fragment convertedNoteToIssueEventFields on ConvertedNoteToIssueEvent {
+          id
+          actor {
+            ...actorFields
+          }
+          createdAt
+          project{
+            name
+          }
+          projectColumnName
+        }
+      `
+    }
+
+    static commentDeletedEventFields() {
+      return `
+        fragment commentDeletedEventFields on CommentDeletedEvent {
+          id
+          actor {
+            ...actorFields
+          }
+          createdAt
+        }
+      `
+    }
+
+    static demilestonedEventFields() {
+      return `
+        fragment demilestonedEventFields on DemilestonedEvent {
+          id
+          actor {
+            ...actorFields
+          }
+          createdAt
+          milestoneTitle
+        }
+      `
+    }
+
+    static disconnectedEventFields() {
+      return `
+        fragment disconnectedEventFields on DisconnectedEvent {
+          id
+          actor {
+            ...actorFields
+          }
+          createdAt
+          source {
+            ... on Issue {
+              number
+            }
+          }
+          subject {
+            ... on PullRequest {
+              number
+            }
+          }
+        }
+      `
+    }
+
+    static issueCommentEventFields() {
+      return `
+        fragment issueCommentEventFields on Comment {
+          id
+          createdAt
+          updatedAt
+          author{
+            login
+            }
+        }
+      `
+    }
+
+    static labeledEventFields() {
+      return `
+        fragment labeledEventFields on LabeledEvent {
+          id
+          actor {
+            ...actorFields
+          }
+          createdAt
+          label{
+            name
+          }
+        }
+      `
+    }
+
+    static milestonedEventFields() {
+      return `
+        fragment milestonedEventFields on MilestonedEvent {
+          id
+          actor {
+            ...actorFields
+          }
+          createdAt
+          milestoneTitle
+        }
+      `
+   }
+
     static movedColumnsInProjectEventFields() {
        return `
          fragment movedColumnsInProjectEventFields on MovedColumnsInProjectEvent {
@@ -86,4 +254,5 @@ export default class IssueTimelineItemsFragments {
          }
        `
     }
+
 }
