a38ba693c07378601c185707b9a97f12e1b90f96
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-08-05 16:56:02 -0700

    pc - hot fix, try fixing url for hooks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.routes.default_url_options[:host] = "#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
+
 end
