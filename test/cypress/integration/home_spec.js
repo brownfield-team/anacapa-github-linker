2593295b6c750e18ab52184706b261d57d5b2c66
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-08-23 09:00:11 -0700

    pc - add cypress tests for home page when logged in as admin
    Tests whether UI elements that are needed when logged in as admin are present

--- test/cypress/integration/home_spec.js
+++ test/cypress/integration/home_spec.js
@@ -36,5 +36,59 @@ describe("Home Page before login", () => {
         .should('contain.text', 'Courses')
     });
   }); // Student login
-  
+
+  describe("Admin login", () => {
+
+    let courseUrl;
+
+    beforeEach(() => { 
+        cy.request("/api/testhooks/login_admin");
+        cy.visit("/courses");
+        cy.get('table#course_list')
+            .find('a')
+            .invoke('attr', 'href')
+            .then((href) => {
+                cy.log(`href=${href}`);
+                courseUrl = href
+            }).then( () => {
+                cy.visit(courseUrl)
+            });
+    });
+
+    it("has a nav bar", () => {
+        cy.get("nav.navbar").should("exist");
+    });
+
+    it("has a Home button", () => {
+        cy.get('a[href="/"]')
+        .should('have.text', 'Home')
+    });
+
+    it("has a Courses button", () => {
+        cy.get('a[href="/courses"]')
+        .should('contain.text', 'Courses')
+    });
+
+    it("has a Users button", () => {
+        cy.get('a[href="/users"]')
+        .should('contain.text', 'Users')
+    });
+
+    it("has an Admin button", () => {
+        cy.get('a[href="/admin/dashboard"]')
+        .should('contain.text', 'Admin')
+    });
+
+    it("has a Students button", () => {
+        cy.get(`a[href="${courseUrl}"]`)
+        .should('contain.text', 'Students')
+    });
+
+    it("has a Project Teams button", () => {
+        cy.get(`a[href="${courseUrl}/project_teams"]`)
+        .should('contain.text', 'Project Teams')
+    });
+
+  }); // Admin login
+
 }); 
