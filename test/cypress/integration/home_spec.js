ca49729b112585b88b2f468dc78600d1e8f2dccd
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-08-23 08:55:17 -0700

    pc - add cypress tests with login as student
    These tests use the testhooks endpoint to simulate logging in
    as a student.  We check that the appropriate UI elements appear on the home page
    after student login.

--- test/cypress/integration/home_spec.js
+++ test/cypress/integration/home_spec.js
@@ -1,16 +1,40 @@
-describe("Home Page Admin", () => {
-    // before(() => {
 
-    // });
+describe("Home Page before login", () => {
+  describe("Before login", () => {
 
-    // beforeEach(() => {
-
-    // });
-
-    it("has a nav bar", () => {
-      cy.visit("/");
-      cy.get("nav.navbar").should("exist");
+    beforeEach(() => {
+        cy.visit("/");
     });
 
+    it("has a nav bar", () => {
+        cy.get("nav.navbar").should("exist");
+    });
 
-  }); 
+    it("has a Home button", () => {
+        cy.get('a[href="/"]')
+        .should('have.text', 'Home')
+    });
+  }); // before login
+
+  describe("Student login", () => {
+    beforeEach(() => { 
+      cy.request("/api/testhooks/login_student");
+      cy.visit("/");
+    });
+
+    it("has a nav bar", () => {
+        cy.get("nav.navbar").should("exist");
+    });
+
+    it("has a Home button", () => {
+        cy.get('a[href="/"]')
+        .should('have.text', 'Home')
+    });
+
+    it("has a Courses button", () => {
+        cy.get('a[href="/courses"]')
+        .should('contain.text', 'Courses')
+    });
+  }); // Student login
+  
+}); 
