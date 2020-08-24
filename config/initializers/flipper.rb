2f89211611e15582c30c03c788faf7848d8672d2
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-08-24 12:03:47 -0700

    pc - configure flipper page and make list for features
    In this commit, we customize the flipper page to remove the 'fun video',
    and to put the current Rails.env value in the banner.
    
    We also make a list of feature toggles that will be initialized to
    disabled when the app is first run.

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -1,6 +1,12 @@
 require 'flipper'
 require 'flipper/adapters/active_record'
 
+# ADD NEW FEATURES TO THE ARRAY BELOW
+
+features = [
+  'assignments' 
+]
+
 Flipper.configure do |config|
   config.default do
     adapter = Flipper::Adapters::ActiveRecord.new
@@ -10,3 +16,26 @@ Flipper.configure do |config|
   end
 end
 
+Flipper::UI.configure do |config|
+  config.fun = false
+  config.banner_text = "Rails.env=#{Rails.env}"
+  config.banner_class = 'danger'
+end
+
+# This setup is primarily for first deployment, because consequently 
+# we can add new features from the Web UI. However when the DB is changed/crashed
+# or in dev mode. This will immediately migrate the default features to be controlled. 
+def setup_features(features)
+  features.each do |feature|
+    if Flipper[feature].exist?
+      return
+    end
+    
+    # Disable feature by default
+    Flipper[feature].disable
+  end
+end
+
+if ActiveRecord::Base.connection.data_source_exists? 'flipper_features'
+  setup_features(features)
+end
