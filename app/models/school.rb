7820ee8aedf148bca4877aa21b5f4b9374fd21d6
Varun Iyer <varun_iyer@posteo.net>
2021-04-11 18:01:11 -0700

    Added a Schools ApplicationRecord class

---
+++ app/models/school.rb
@@ -0,0 +1,5 @@
+class School < ApplicationRecord
+has_many :courses, :join_table => :courses_school
+  validates :name, presence: true, uniqueness: true
+  validates :abbreviation, presence: true, uniqueness: true
+end
