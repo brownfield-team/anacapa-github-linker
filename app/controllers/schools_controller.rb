683dd83107d10ebe2e7f1c49f1325a863e9c7c7f
Varun Iyer <varun_iyer@posteo.net>
2021-04-18 23:32:59 -0700

    Added appropriate controller authorization for Schools

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,6 +1,7 @@
 class SchoolsController < ApplicationController
   before_action :set_school, only: [:show, :edit, :update, :destroy]
 
+  load_and_authorize_resource
   # GET /schools
   # GET /schools.json
   def index
