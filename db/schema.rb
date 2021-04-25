d6ebc17d53de5f7dcfcacf480ace7883ce4c7621
Varun Iyer <varun_iyer@posteo.net>
2021-04-25 16:36:54 -0700

    Schema commit.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_164332) do
+ActiveRecord::Schema.define(version: 2021_04_19_160808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 2021_04_15_164332) do
     t.datetime "updated_at", null: false
     t.boolean "hidden"
     t.boolean "github_webhooks_enabled"
+    t.string "term"
+    t.bigint "school_id"
+    t.index ["school_id"], name: "index_courses_on_school_id"
   end
 
   create_table "flipper_features", force: :cascade do |t|
@@ -279,6 +282,7 @@ ActiveRecord::Schema.define(version: 2021_04_15_164332) do
 
   add_foreign_key "completed_jobs", "courses"
   add_foreign_key "completed_jobs", "github_repos"
+  add_foreign_key "courses", "schools"
   add_foreign_key "github_repos", "courses"
   add_foreign_key "org_webhook_events", "courses"
   add_foreign_key "org_webhook_events", "github_repos"
