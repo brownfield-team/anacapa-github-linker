e59ee73996d46e5fcca2d9141ad794207678dff4
Varun Iyer <varun_iyer@posteo.net>
2021-05-27 08:43:16 -0700

    Created fixtures + fixed tests for sprints
    Ongoing issue: instructor/admin privileges to index

--- test/controllers/sprints_controller_test.rb
+++ test/controllers/sprints_controller_test.rb
@@ -1,48 +1,69 @@
 require 'test_helper'
 
 class SprintsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @sprint = sprints(:one)
+    @course = courses(:course1)
+    @sprint = sprints(:sprint1)
+    @admin = users(:wes)
+    @admin.add_role(:admin)
+    @instructor = users(:tim)
+    @instructor.add_role(:julie)
+    @nopriv = users(:julie)
   end
 
   test "should get index" do
-    get sprints_url
+    sign_in @instructor
+    get course_sprints_url(@course)
     assert_response :success
   end
 
+  test "should not get index" do
+    sign_in @nopriv
+    get course_sprints_url(@course)
+    assert_redirected_to root_path
+  end
+
   test "should get new" do
-    get new_sprint_url
+    sign_in @instructor
+    get new_course_sprint_url(@course)
     assert_response :success
   end
 
   test "should create sprint" do
+    sign_in @instructor
     assert_difference('Sprint.count') do
-      post sprints_url, params: { sprint: { course_id: @sprint.course_id, end_date: @sprint.end_date, name: @sprint.name, start_date: @sprint.start_date } }
+      post course_sprints_url(@course), params: { sprint: { course_id: @sprint.course_id, end_date: @sprint.end_date, name: @sprint.name, start_date: @sprint.start_date } }
     end
 
-    assert_redirected_to sprint_url(Sprint.last)
+    assert_redirected_to edit_course_url(@course)
   end
 
   test "should show sprint" do
-    get sprint_url(@sprint)
+    sign_in @instructor
+    get course_sprint_url(@course, @sprint)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_sprint_url(@sprint)
+    sign_in @instructor
+    get edit_course_sprint_url(@course, @sprint)
     assert_response :success
   end
 
   test "should update sprint" do
-    patch sprint_url(@sprint), params: { sprint: { course_id: @sprint.course_id, end_date: @sprint.end_date, name: @sprint.name, start_date: @sprint.start_date } }
-    assert_redirected_to sprint_url(@sprint)
+    sign_in @instructor
+    patch course_sprint_url(@course, @sprint), params: { sprint: { course_id: @sprint.course_id, end_date: @sprint.end_date, name: @sprint.name, start_date: @sprint.start_date } }
+    assert_response :success
   end
 
   test "should destroy sprint" do
+    sign_in @instructor
     assert_difference('Sprint.count', -1) do
-      delete sprint_url(@sprint)
+      delete course_sprint_url(@course, @sprint)
     end
 
-    assert_redirected_to sprints_url
+    assert_redirected_to edit_course_url(@course)
   end
 end
