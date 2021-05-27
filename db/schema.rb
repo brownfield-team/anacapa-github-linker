66fab8efc855efcd287fe74e940077a4a4fbdbd2
Faiz Surani <faiz.surani@gmail.com>
2021-05-26 23:21:50 -0700

    Add migration for external column on github_repos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_160808) do
+ActiveRecord::Schema.define(version: 2021_05_27_061417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_160808) do
     t.string "full_name"
     t.string "visibility"
     t.boolean "is_project_repo"
+    t.boolean "external"
     t.index ["course_id"], name: "index_github_repos_on_course_id"
   end
 
