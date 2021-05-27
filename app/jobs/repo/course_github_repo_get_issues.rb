8d7a31629e42ba69ec244020432bacc0c65d1e2e
Faiz Surani <faiz.surani@gmail.com>
2021-05-27 11:57:13 -0700

    Fix repo jobs to use explicit repo organization instead of course organization

--- app/jobs/repo/course_github_repo_get_issues.rb
+++ app/jobs/repo/course_github_repo_get_issues.rb
@@ -6,15 +6,15 @@ class CourseGithubRepoGetIssues < CourseGithubRepoJob
     @job_description = "Get issues for this repo"
     @github_repo_full_name
 
-    # defJobResult.newh(total, new_count, updated_count) 
-    #   { 
+    # defJobResult.newh(total, new_count, updated_count)
+    #   {
     #     "total_issues" => total,
     #     "total_new_issues" => new_count,
     #     "total_updated_issues" => updated_count
     #   }
     # end
 
-    # def combine_results(h1,h2) 
+    # def combine_results(h1,h2)
     #   {
     #     "total_issues" => h1["total_issues"] + h2["total_issues"],
     #     "total_new_issues" => h1["total_new_issues"] + h2["total_new_issues"],
@@ -27,12 +27,12 @@ class CourseGithubRepoGetIssues < CourseGithubRepoJob
     # end
 
     def attempt_job(options)
-      @github_repo_full_name = "#{@course.course_organization}/#{@github_repo.name}"      
+      @github_repo_full_name = @github_repo.full_name
       final_results = JobResult.new
       more_pages = true
       end_cursor = ""
       while more_pages
-        query_results = perform_graphql_query(@github_repo.name,@course.course_organization,end_cursor)
+        query_results = perform_graphql_query(@github_repo.name, @github_repo.organization, end_cursor)
         begin
           more_pages = query_results[:data][:repository][:issues][:pageInfo][:hasNextPage]
           end_cursor = query_results[:data][:repository][:issues][:pageInfo][:endCursor]
@@ -44,7 +44,7 @@ class CourseGithubRepoGetIssues < CourseGithubRepoJob
         final_results = final_results + results
       end
       "Issues retrieved for Course: #{@course.name} Repo: #{@github_repo.name} <br/>      #{final_results.report}"
-    end  
+    end
 
     def store_issues_in_database(issues)
       total_new_issues = 0
@@ -54,7 +54,7 @@ class CourseGithubRepoGetIssues < CourseGithubRepoJob
         if existing_issue
           total_updated_issues += update_one_issue(existing_issue, i)
         else
-          total_new_issues += store_one_issue_in_database(i) 
+          total_new_issues += store_one_issue_in_database(i)
         end
       }
     JobResult.new(issues.length,total_new_issues,total_updated_issues)
@@ -80,7 +80,7 @@ class CourseGithubRepoGetIssues < CourseGithubRepoJob
         ""
       end
     end
-    
+
     def column_project_url(x)
       begin
         x[:column][:project][:url]
@@ -132,7 +132,7 @@ class CourseGithubRepoGetIssues < CourseGithubRepoJob
         puts "***ERROR*** update_issue_fields issue #{sawyer_resource_to_s(i)} @github_repo #{@github_repo}"
         return 0
       end
-      
+
       begin  # "try" block
         username = i[:author][:login]
         issue.author_login = username
@@ -140,7 +140,7 @@ class CourseGithubRepoGetIssues < CourseGithubRepoJob
       rescue # optionally: `rescue Exception => ex`
         username = ""
         issue.roster_student = nil
-      end 
+      end
 
       begin
         issue.save!
@@ -171,7 +171,7 @@ class CourseGithubRepoGetIssues < CourseGithubRepoJob
         if after != ""
           after_clause=", after: \"#{after}\""
         else
-          after_clause = ""  
+          after_clause = ""
         end
         # Although the published limit on Graphql queries is 100,
         # in practice, we've found that it sometimes fails.
@@ -179,7 +179,7 @@ class CourseGithubRepoGetIssues < CourseGithubRepoJob
 
         # TODO: Add closed and closedAt to database
         # TODO: Get body, and parse for checkboxes, complete and incomplete
-        # TODO: Add number of assignees, and try to tie assignees back to 
+        # TODO: Add number of assignees, and try to tie assignees back to
         #   roster students if possible.
 
         <<-GRAPHQL
@@ -245,9 +245,8 @@ class CourseGithubRepoGetIssues < CourseGithubRepoJob
       begin
         result = sawyer_resource.to_hash.to_s
       rescue
-        result = sawyer_resource.to_s 
+        result = sawyer_resource.to_s
       end
       result
     end
 end
-  
