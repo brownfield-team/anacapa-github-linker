b58576dbe1d1ac21cd38066f4601c45134bec4af
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-06-15 19:24:26 -0700

    pc/vi - WIP on converting courses page to React Bootstrap Table

--- app/javascript/components/Courses/CoursesIndex.jsx
+++ app/javascript/components/Courses/CoursesIndex.jsx
@@ -1,19 +1,81 @@
 import React, { Component, Fragment } from 'react';
-import CoursesIndexItem from "./CoursesIndexItem"
-import * as PropTypes from 'prop-types';
 import axios from "axios";
-import {coursesRoute} from "../../services/service-routes";
+import { coursesRoute } from "../../services/service-routes";
 import ReactOnRails from "react-on-rails";
+import BootstrapTable from 'react-bootstrap-table-next';
+import { Button } from "react-bootstrap";
 
 class CoursesIndex extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {courses: []};
+		this.state = { courses: [] };
 		const csrfToken = ReactOnRails.authenticityToken();
-        axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
-        axios.defaults.params = {}
-        axios.defaults.params['authenticity_token'] = csrfToken;
+		axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
+		axios.defaults.params = {}
+		axios.defaults.params['authenticity_token'] = csrfToken;
 	}
+
+
+	 schoolDisplay = () => this.props.course.school ? this.props.course.school.abbreviation : ""; 
+
+	 renderCourseUrl = (cell, row) => <a href={row.path}>{cell}</a>;
+
+	 renderCourseOrgLink = (cell, row) => {
+		const url = `https://github.com/${cell}`;
+		return (
+			<a href={url}>{cell}</a>
+		)
+	};
+
+	 renderEditButton = (cell, row) => {
+		return (
+			<Button variant="warning" data-testid={`edit-button-${row.id}`} href={`${row.edit_path}`} >Edit</Button>
+		)
+	}
+
+	 renderDeleteButton = (cell, row) => {
+		return (
+			<Button variant="danger" data-testid={`delete-button-${row.id}`} onClick={() => this.deleteCourse(row)} >Delete</Button>
+		)
+	}
+
+	 columns = [
+		{
+			dataField: 'school.abbreviation',
+			text: 'School',
+			sort: true
+		}, {
+			dataField: 'name',
+			text: 'Name',
+			sort: true,
+			formatter: (cell, row) => this.renderCourseUrl(cell, row)
+		}, {
+			dataField: 'term',
+			text: 'Term',
+			sort: true
+		}, {
+			dataField: 'hidden',
+			text: 'Hidden',
+			sort: true
+		}, {
+			dataField: 'course_organization',
+			text: 'Course Organization',
+			sort: true,
+			formatter: (cell, row) => this.renderCourseOrgLink(cell, row)
+		}, {
+			dataField: "edit",
+			text: "Edit",
+			isDummyField: true,
+			formatter: (cell, row) => this.renderEditButton(cell, row),
+			hidden: false /* should be true if this is not an instructor / admin */
+		}, {
+			dataField: "delete",
+			text: "Delete",
+			isDummyField: true,
+			formatter: (cell, row) => this.renderDeleteButton(cell, row),
+			hidden: false /* should be true if this is not an instructor / admin */
+		}
+	];
 
 
 	componentDidMount() {
@@ -22,7 +84,7 @@ class CoursesIndex extends Component {
 
 	getCourses = () => {
 		axios.get(coursesRoute).then(response => {
-			this.setState({courses: response.data});
+			this.setState({ courses: response.data });
 		});
 	}
 
@@ -32,29 +94,39 @@ class CoursesIndex extends Component {
 
 	render() {
 		return (
-			<Fragment>
-				<div>
-					<div className="panel panel-body">
-						<table className="bootstrap-table" data-toggle="table" data-search="true" data-pagination="true" data-show-columns="true" data-show-columns-toggle-all="true" data-show-pagination-switch="true" data-filter-control="true">
-							<thead>
-								<tr>
-									<th scope="col">School</th>
-									<th scope="col">Name</th>
-									<th scope="col">Term</th>
-									<th scope="col">Hidden</th>
-									<th scope="col">GitHub Organization</th>
-									<th scope="col" colSpan="2">Actions</th>
-								</tr>
-							</thead>
-							<tbody>
-								{this.state.courses.map(course =>
-									<CoursesIndexItem course={course} onDelete={this.deleteCourse} key={course.id}/>
-								)}
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</Fragment>
+
+			<BootstrapTable
+				boostrap4={true}
+				columns={this.columns}
+				data={this.state.courses}
+				keyField="id"
+			/>
+
+			// <Fragment>
+			// 	<div>
+			// 		<div className="panel panel-body">
+			// 			<table className="bootstrap-table" data-toggle="table" data-search="true" data-pagination="true" data-show-columns="true" data-show-columns-toggle-all="true" data-show-pagination-switch="true" data-filter-control="true">
+			// 				<thead>
+			// 					<tr>
+			// 						<th scope="col">School</th>
+			// 						<th scope="col">Name</th>
+			// 						<th scope="col">Term</th>
+			// 						<th scope="col">Hidden</th>
+			// 						<th scope="col">GitHub Organization</th>
+			// 						<th scope="col" colSpan="2">Actions</th>
+			// 					</tr>
+			// 				</thead>
+			// 				<tbody>
+			// 					{this.state.courses.map(course =>
+			// 						<CoursesIndexItem course={course} onDelete={this.deleteCourse} key={course.id} />
+			// 					)}
+			// 				</tbody>
+			// 			</table>
+			// 		</div>
+			// 	</div>
+			// </Fragment>
+
+
 		);
 	}
 }
