e1bb0fe288f92668587803bd10cf94879a7c6077
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-07-21 09:35:54 -0700

    pc - restore code accidentally deleted from /api/courses/org_teas_controller.rb

--- app/controllers/api/courses/org_teams_controller.rb
+++ app/controllers/api/courses/org_teams_controller.rb
@@ -2,11 +2,29 @@ module Api::Courses
   class OrgTeamsController < ApplicationController
     respond_to :json
     load_and_authorize_resource :course
+    load_and_authorize_resource
 
     def index
-      response = { course: @course }
-      respond_with response
+      respond_with @course.org_teams
     end
 
+    def show
+      members = @org_team.student_team_memberships.map{ |s| { 
+          membership: s, 
+          full_name: s.roster_student.full_name, 
+          github_id: s.roster_student.user.username 
+        } 
+      }
+
+      repos = @org_team.repo_team_contributors.map{ |r| {
+          repo: r,
+          repo_name: r.github_repo.name,
+          permission: r.permission_level
+        }
+      }
+
+      response = { org_team: @org_team, members: members, course: @course, repos: repos }
+      respond_with response
+    end
   end
 end
