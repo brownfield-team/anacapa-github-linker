bd66c535fbebc39b3fe794791c6c909e5ad4d1cb
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-07-22 10:49:17 -0700

    pc/team - fix two bugs in code to assign orphan names and emails
    (1) Assigning by name was not using the utility to check whether the fix orphan commits job is already running.
        But now it does do that.
    (2) The code to check whether the Fix Orphan Commits job was already running was not checking by course id,
        thus, if the job were running for some other course, it was incorrectly assume that the job does not
        need to be requeued.

--- app/controllers/courses/orphan_commits_controller.rb
+++ app/controllers/courses/orphan_commits_controller.rb
@@ -17,7 +17,7 @@ module Courses
       message = "Assigning orphan name '#{params[:name]}' to roster student #{params[:roster_student_id]} (#{roster_student.full_name}), Fix Orphan Commits Job launched "
       Rails.logger.info message
       on = OrphanName.new(name: params[:name], course:@course, roster_student_id: params[:roster_student_id]).save!
-      FixOrphanCommitsJob.perform_async(@course.id)
+      message += requeueOrphanCommitsIfNotRunning
       redirect_to course_orphan_commits_path(@course), notice: message   
     end
 
@@ -30,8 +30,10 @@ module Courses
       redirect_to course_orphan_commits_path(@course), notice: message   
     end
 
+    private
+
     def requeueOrphanCommitsIfNotRunning
-      latestOrphanCommitsJob  = CompletedJob.where(job_name: "Fix Orphan Commits").order("created_at DESC").last
+      latestOrphanCommitsJob  = CompletedJob.where(course_id: @course.id, job_name: "Fix Orphan Commits").order("created_at DESC").last
       if latestOrphanCommitsJob.nil? || latestOrphanCommitsJob.summary != "In progress"
         FixOrphanCommitsJob.perform_async(@course.id)
         "Fix Orphan Commits Job launched "
