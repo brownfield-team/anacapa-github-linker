1b0c4de472e10b4e844cde8ac7c79debcd1c810c
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-07-22 10:36:17 -0700

    pc/team - remove unused code in api controller

--- app/controllers/api/courses/orphan_emails_controller.rb
+++ app/controllers/api/courses/orphan_emails_controller.rb
@@ -11,9 +11,5 @@ module Api::Courses
         paginate json: @course.orphans_by_email(params[:email])
       end
 
-      def assign
-        Rails.logger.info "Assigning Orphan Email: #{params[:email]} to roster_student_id: #{params[:roster_student_id]}"
-        on = OrphanEmail.new(email: params[:email], course:@course, roster_student_id: params[:roster_student_id])        
-      end
     end
   end
