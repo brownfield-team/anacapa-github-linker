dd87381eaaddc4c0037ba14f183c74a39f109db7
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-07-23 11:09:32 -0700

    pc/at - simplify two React components by removing used code

--- app/javascript/components/Course/OrphanCommits/AssignOrphanEmailToRosterStudent.jsx
+++ app/javascript/components/Course/OrphanCommits/AssignOrphanEmailToRosterStudent.jsx
@@ -1,42 +1,11 @@
 import React, { Component } from 'react';
-import axios from "../../../helpers/axios-rails"
-import ReactOnRails from "react-on-rails";
 import RosterStudentAssign from '../../RosterStudents/RosterStudentAssign';
-import { orphanEmailsRoute } from "../../../services/service-routes";
 
 
 class AssignOrphanEmailToRosterStudent extends Component {
 
     constructor(props) {
         super(props);
-
-        const csrfToken = ReactOnRails.authenticityToken();
-        axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
-        axios.defaults.params = {}
-        axios.defaults.params['authenticity_token'] = csrfToken;
-        this.state = {};
-    }
-
-    updateOrphanEmails = () => {
-
-        const url = orphanEmailsRoute(this.props.course_id);
-        const params = {};
-        // self=this Otherwise, calling setState fails because the scope for "this" is the success/error function.
-        const self = this;
-        Rails.ajax({
-            url: url,
-            type: "get",
-            data: $.param(params),
-            beforeSend: function () {
-                return true;
-            },
-            success: function (data, status, xhr) {
-                self.setState(prevState => ({ ...prevState, orphanEmails: data, orphanEmailsError: "" }));
-            },
-            error: function (data) {
-                self.setState(prevState => ({ ...prevState, orphanEmails: [], orphanEmailsError: data }));
-            }
-        });
     }
 
     assign_url = (course_id, email, roster_student_id) => `/courses/${course_id}/orphan_commits_by_email/assign?email=${encodeURIComponent(email)}&roster_student_id=${roster_student_id}`;
--- app/javascript/components/Course/OrphanCommits/AssignOrphanNameToRosterStudent.jsx
+++ app/javascript/components/Course/OrphanCommits/AssignOrphanNameToRosterStudent.jsx
@@ -1,44 +1,13 @@
 import React, { Component } from 'react';
-import axios from "../../../helpers/axios-rails"
-import ReactOnRails from "react-on-rails";
 import RosterStudentAssign from '../../RosterStudents/RosterStudentAssign';
-import { orphanNamesRoute } from "../../../services/service-routes";
 
 class AssignOrphanNameToRosterStudent extends Component {
 
     constructor(props) {
-
         super(props);
-
-        const csrfToken = ReactOnRails.authenticityToken();
-        axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
-        axios.defaults.params = {}
-        axios.defaults.params['authenticity_token'] = csrfToken;
-
-        this.state = {};
     }
 
-    updateOrphanNames = () => {
-        const url = orphanNamesRoute(this.props.course_id);
-        const params = {};
-        // self=this; Otherwise, calling setState fails because the scope for "this" is the success/error function.
-        const self = this;
-        Rails.ajax({
-            url: url,
-            type: "get",
-            data: $.param(params),
-            beforeSend: function () {
-                return true;
-            },
-            success: function (data, status, xhr) {
-                self.setState(prevState => ({ ...prevState, orphanNames: data, orphanNamesError: "" }));
-            },
-            error: function (data) {
-                self.setState(prevState => ({ ...prevState, orphanNames: [], orphanNamesError: data }));
-            }
-        });
-    }
-
+   
     assign_url = (course_id, name, roster_student_id) => `/courses/${course_id}/orphan_commits_by_name/assign/${encodeURIComponent(name)}/to/${roster_student_id}`;
 
     render() {
