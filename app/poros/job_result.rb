520a42f00e86bab559e5241c654e64baf1bdc511
Ahsun Tariq <ahsun.tariq@hotmail.com>
2021-08-02 10:21:19 -0700

    at/pc/bh we fixed issues to include date ranges for the course window.

--- app/poros/job_result.rb
+++ app/poros/job_result.rb
@@ -1,9 +1,10 @@
 class JobResult
     
-    def initialize(total=0,created=0,updated=0)
+    def initialize(total=0,created=0,updated=0,excluded=0)
         @total = total
         @created = created
         @updated = updated
+        @excluded = excluded
     end
 
     def total
@@ -18,20 +19,25 @@ class JobResult
         @updated
     end
 
-    def add(total, created, updated)
+    def excluded
+        @excluded
+    end
+
+    def add(total, created, updated, excluded)
         @total += total
         @created += created
         @updated += updated
+        @excluded += excluded
         self
     end
 
     def all_good?
-        @total == @created + @updated
+        @total == @created + @updated  + @excluded
     end
 
     def report
-        job_outcome = all_good? ? "Successfully" : "with errors; CHECK LOG; note that total items retrieved does NOT MATCH number created + updated"
-        "Job Completed #{job_outcome}.<br />        Retrieved #{@total} items. Created #{@created}, Updated #{@updated} in database"
+        job_outcome = all_good? ? "Successfully" : "with errors; CHECK LOG; note that total items retrieved does NOT MATCH number created + updated + excluded"
+        "Job Completed #{job_outcome}.<br />        Retrieved #{@total} items. Created #{@created}, Updated #{@updated}, Excluded #{@excluded} in database"
     end
 
     def +(obj) 
@@ -39,7 +45,8 @@ class JobResult
         result.add(
             self.total + obj.total,
             self.created + obj.created,
-            self.updated + obj.updated
+            self.updated + obj.updated,
+            self.excluded + obj.excluded
         )
         result
     end
