c05f0c30d8630cff52d88745d3e711376859bad9
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-08-02 09:43:11 -0700

    pc/team - remove commented out code in migration

--- db/migrate/20210727191054_add_fields_to_repo_commit_event.rb
+++ db/migrate/20210727191054_add_fields_to_repo_commit_event.rb
@@ -1,12 +1,3 @@
-# class AddFieldsToRepoCommitEvent < ActiveRecord::Migration[5.2]
-#   def change
-#     add_column :repo_commit_events, :package_lock_json_files_changed, :integer
-#     add_column :repo_commit_events, :package_lock_json_additions, :integer
-#     add_column :repo_commit_events, :package_lock_json_deletions, :integer
-#   end
-# end
-
-
 class AddFieldsToRepoCommitEvent < ActiveRecord::Migration[5.2]
   def change
     add_column :repo_commit_events, :files_json, :string
