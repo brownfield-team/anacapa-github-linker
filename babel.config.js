5b2776753662822ed43569eea632a3aff1068508
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-08-19 17:01:06 -0700

    pc - try upgrading webpacker

--- babel.config.js
+++ babel.config.js
@@ -22,10 +22,8 @@ module.exports = function(api) {
         {
           targets: {
             node: 'current'
-          },
-          modules: 'commonjs'
-        },
-        '@babel/preset-react'
+          }
+        }
       ],
       (isProductionEnv || isDevelopmentEnv) && [
         '@babel/preset-env',
@@ -35,13 +33,6 @@ module.exports = function(api) {
           corejs: 3,
           modules: false,
           exclude: ['transform-typeof-symbol']
-        }
-      ],
-      [
-        '@babel/preset-react',
-        {
-          development: isDevelopmentEnv || isTestEnv,
-          useBuiltIns: true
         }
       ]
     ].filter(Boolean),
@@ -55,12 +46,6 @@ module.exports = function(api) {
         {
           loose: true
         }
-      ],
-      [
-          "@babel/plugin-proposal-private-property-in-object", { "loose": true }
-      ],
-      [
-        "@babel/plugin-proposal-private-methods", { "loose": true }
       ],
       [
         '@babel/plugin-proposal-object-rest-spread',
@@ -80,12 +65,6 @@ module.exports = function(api) {
         '@babel/plugin-transform-regenerator',
         {
           async: false
-        }
-      ],
-      isProductionEnv && [
-        'babel-plugin-transform-react-remove-prop-types',
-        {
-          removeImport: true
         }
       ]
     ].filter(Boolean)
