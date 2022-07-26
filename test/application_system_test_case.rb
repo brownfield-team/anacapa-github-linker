901a8ce0c20ea82d6147f4c7d083496732e06c45
Adrian Lindell <lindell@ucsb.edu>
2022-07-26 12:21:46 -0700

    al - updated Selenium driver to use options instead of profile due to deprecation warning

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,12 +4,10 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   DOWNLOAD_PATH = Rails.root.join("tmp/downloads").to_s
 
   Capybara.register_driver :chrome_driver do |app|
-    profile = Selenium::WebDriver::Chrome::Profile.new
-    profile["download.default_directory"] = DOWNLOAD_PATH
-    Capybara::Selenium::Driver.new(app, :browser => :chrome, :profile => profile)
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_preference('download.default_directory', DOWNLOAD_PATH)
+    Capybara::Selenium::Driver.new(app, :browser => :chrome, :options => options)
   end
-
-  Capybara.default_driver = Capybara.javascript_driver = :chrome
 
   driven_by :chrome_driver, using: :chrome, screen_size: [1400, 1400]
 end
