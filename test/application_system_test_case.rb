6aaecca4808742868357faa211b55656a21d2c22
Adrian Lindell <lindell@ucsb.edu>
2022-07-26 11:51:29 -0700

    al - download csv to tmp/downloads and verify headers

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,15 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  DOWNLOAD_PATH = Rails.root.join("tmp/downloads").to_s
+
+  Capybara.register_driver :chrome_driver do |app|
+    profile = Selenium::WebDriver::Chrome::Profile.new
+    profile["download.default_directory"] = DOWNLOAD_PATH
+    Capybara::Selenium::Driver.new(app, :browser => :chrome, :profile => profile)
+  end
+
+  Capybara.default_driver = Capybara.javascript_driver = :chrome
+
+  driven_by :chrome_driver, using: :chrome, screen_size: [1400, 1400]
 end
--- test/system/courses_test.rb
+++ test/system/courses_test.rb
@@ -20,5 +20,13 @@ class CoursesTest < ApplicationSystemTestCase
     sleep 1
     
     csv_name = "#{@course2.name.parameterize}-students-#{Date.today}.csv"
+
+    full_path = DOWNLOAD_PATH+"/"+csv_name
+    puts full_path
+
+    assert File.exist?(full_path)
+    actual_headers = CSV.open(full_path, 'r') { |csv| csv.first.to_s }
+
+    assert_equal("[\"student_id\", \"email\", \"first_name\", \"last_name\", \"enrolled\", \"section\", \"github_username\", \"slack_uid\", \"slack_username\", \"slack_display_name\", \"org_status\", \"teams\"]",actual_headers,"Header does not match")
   end
 end
