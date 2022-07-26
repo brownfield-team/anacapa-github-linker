cc4cbed8d4bd96882b17a5670799ec671aeef4ce
Adrian Lindell <lindell@ucsb.edu>
2022-07-26 14:26:32 -0700

    al - download roster as JSON and fixed bug to remove duplicate downloads

--- test/system/courses_test.rb
+++ test/system/courses_test.rb
@@ -14,17 +14,34 @@ class CoursesTest < ApplicationSystemTestCase
 
   test "instructors can download student roster as CSV" do
     visit course_path(@course2)
-
+    
+    csv_name = "#{@course2.name.parameterize}-students-#{Date.today}.csv"
+    
+    full_path = DOWNLOAD_PATH+"/"+csv_name
+    
+    File.delete(full_path) if File.exist?(full_path)
+      
     click_on "Download Roster (CSV)"
     sleep 1
     
-    csv_name = "#{@course2.name.parameterize}-students-#{Date.today}.csv"
-
-    full_path = DOWNLOAD_PATH+"/"+csv_name
-
     assert File.exist?(full_path)
     actual_headers = CSV.open(full_path, 'r') { |csv| csv.first.to_s }
 
     assert_equal("[\"student_id\", \"email\", \"first_name\", \"last_name\", \"enrolled\", \"section\", \"github_username\", \"slack_uid\", \"slack_username\", \"slack_display_name\", \"org_status\", \"teams\"]",actual_headers,"Header does not match")
+  end
+
+  test "instructors can download student roster as JSON" do
+    visit course_path(@course2)
+    
+    json_name = "#{@course2.name.parameterize}-students-#{Date.today}.json"
+    
+    full_path = DOWNLOAD_PATH+"/"+json_name
+
+    File.delete(full_path) if File.exist?(full_path)
+
+    click_on "Download Roster (JSON)"
+    sleep 1
+
+    assert File.exist?(full_path)
   end
 end
