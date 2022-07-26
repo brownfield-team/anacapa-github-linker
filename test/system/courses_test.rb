49c1175f6564828fa13f416145ed4352fcd39599
Adrian Lindell <lindell@ucsb.edu>
2022-07-26 11:26:53 -0700

    al - use test hook to login for courses system test

--- test/system/courses_test.rb
+++ test/system/courses_test.rb
@@ -1,10 +1,15 @@
 require "application_system_test_case"
 
 class CoursesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit courses_url
-  #
-  #   assert_selector "h1", text: "Course"
-  # end
+  setup do
+    @course2 = courses(:course2)
+    visit "/test_hooks/login_admin"
+  end
+
+  test "visiting the index" do
+    visit courses_url
+  
+    assert_selector "h1", text: "Course"
+  end
 
 end
