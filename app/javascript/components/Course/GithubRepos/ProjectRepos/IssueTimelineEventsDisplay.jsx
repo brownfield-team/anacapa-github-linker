1a070274091acdeaf606fb5b60e9e4cf492469c2
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-12-15 16:51:11 -0800

    pc/at - a good start at getting issue timeline events

--- app/javascript/components/Course/GithubRepos/ProjectRepos/IssueTimelineEventsDisplay.jsx
+++ app/javascript/components/Course/GithubRepos/ProjectRepos/IssueTimelineEventsDisplay.jsx
@@ -3,6 +3,11 @@ import IssueTimelineEventsTable from './IssueTimelineEventsTable';
 import PropTypes from 'prop-types';
 
 import GraphqlQuery from "services/graphql-query";
+import { graphqlRoute } from "services/service-routes";
+
+import IssueTimelineItems from "graphql/IssueTimelineItems";
+import cloneDeep from "lodash.clonedeep"
+import JSONPrettyPanel from 'components/Utilities/JsonPrettyPanel';
 
 import isEqual from 'lodash.isequal';
 
@@ -13,7 +18,8 @@ class IssueTimelineEventsDisplay extends Component {
         GraphqlQuery.csrf_token_fix();
         this.state = {
             repos:null,
-            events:null
+            events: [],
+            timeline_query_results: {},
         }
     }
 
@@ -24,10 +30,55 @@ class IssueTimelineEventsDisplay extends Component {
         }
     }
    
+    courseId = () => this.props.course.id;
+    orgName = () => this.props.course.course_organization;
+    repoName = (repo) => repo.name;
+
     updateEvents = () => {
+        const repos = this.props.repos;
+
         console.log("updateEvents was called");
         console.log("this.props is",this.props);
         console.log("this.props.repos is",this.props.repos);
+
+        const url = graphqlRoute(this.courseId());
+
+        console.log("url=",url);
+
+        repos.forEach( (repo) => {
+            console.log("repo=",repo);
+            let allEvents = [];
+            let tlQuery = IssueTimelineItems.query(this.orgName(), this.repoName(repo), "");
+            let tlAccept =  IssueTimelineItems.accept();
+
+            let setIssueTimelineItems = (o) => {
+                let new_timeline_query_results = this.state.timeline_query_results;
+                let new_errors = this.state.errors;
+
+                if(o.success) {
+                    try {
+                        new_timeline_query_results[this.repoName(repo)] = cloneDeep(o);
+                        let thisReposEvents = IssueTimelineItems.computeIssueTimelineEventsTable(o.data, this.props.databaseId_to_team);
+                        allEvents = allEvents.concat(thisReposEvents.events);
+                    } catch (e) {
+                        new_errors[this.repoName(repo)].push(errorToObject(e))
+                    }
+                } else {
+                    new_errors[this.repoName(repo)].push(o.error)
+                }
+
+                this.setState({
+                    timeline_query_results: new_timeline_query_results,
+                    events: allEvents,
+                    errors: new_errors,
+                });
+            }
+
+            let timelineItemsQueryObject = new GraphqlQuery(url,tlQuery,tlAccept,setIssueTimelineItems,{repo: repo.name});
+            timelineItemsQueryObject.post();
+    
+        });
+
         console.log("updateEvents is finished");
     }
 
@@ -37,7 +88,19 @@ class IssueTimelineEventsDisplay extends Component {
         return (
             <>
                 <h2>Issue Timeline Events</h2>
-                <IssueTimelineEventsTable events={[]} course={this.props.course} />
+                <IssueTimelineEventsTable events={this.state.events } course={this.props.course} />
+                <JSONPrettyPanel
+                    expression={"this.state.timeline_query_results"}
+                    value={this.state.timeline_query_results}
+                />
+                 <JSONPrettyPanel
+                    expression={"this.state.errors"}
+                    value={this.state.errors}
+                />
+                 <JSONPrettyPanel
+                    expression={"this.state.events"}
+                    value={this.state.events}
+                />
             </>              
         );
     }
--- app/javascript/graphql/IssueTimelineItems.js
+++ app/javascript/graphql/IssueTimelineItems.js
@@ -3,17 +3,20 @@ import PageInfoFragments from './fragments/PageInfoFragments';
 import IssueFragments from './fragments/IssueFragments';
 import ActorFragments from './fragments/ActorFragments';
 import IssueTimelineItemsFragments from './fragments/issues/IssueTimelineItemsFragments';
-import vectorToCounts, {combineCounts} from '../utilities/vectorToCounts';
+import vectorToCounts, { combineCounts } from '../utilities/vectorToCounts';
+import IssueUserContentEditFragments from './fragments/issues/IssueUserContentEditFragments';
 class IssueTimelineItems extends GithubGraphqlQuery {
 
-    static accept() { 
-        return "application/vnd.github.starfox-preview+json"; 
+    static accept() {
+        return "application/vnd.github.starfox-preview+json";
     }
-    
+
     static query(org_name, repo_name, after_clause) {
-        return  ( /* GraphQL */ ` 
+        return ( /* GraphQL */ ` 
         query {
             repository(owner: "${org_name}", name: "${repo_name}") {
+                owner { ... actorFields }
+                name
                 issues(first: 50 ${after_clause}) {
                     pageInfo { ...pageInfoFields }
                     totalCount
@@ -39,7 +42,49 @@ class IssueTimelineItems extends GithubGraphqlQuery {
 
         `); /* EndGraphQL */
     }
-    static computeStats (data, databaseId_to_team){
+
+    static computeIssueTimelineEventsTable(data, databaseId_to_team) {
+        let events = [];
+        let errors = {};
+
+        try {
+            let org = data.data.repository.owner.login;
+            let issueNodes = data.data.repository.issues.nodes;
+
+            issueNodes.forEach((n) => {
+                let thisNodesTimelineItems = n.timelineItems.nodes;
+                let thisNodesEvents = thisNodesTimelineItems.map(
+                    (item) => {
+                        return {
+                            org: org,
+                            team: databaseId_to_team[n.author.databaseId],
+                            repo: data.data.repository.name,
+                            actor: item.actor.login,
+                            eventType: item.__typename,
+                            createdAt: item.createdAt,
+                            assigneeUsername: "TODO",
+                            projectName: "TODO",
+                            previousProjectColumnName: "TODO",
+                            projectColumnName: "TODO"
+                        }
+                    });
+                events = events.concat(thisNodesEvents);
+            });
+        } catch (e) {
+            errors = {
+                name: e.name,
+                message: e.message
+            };
+        }
+
+        return {
+            events: events,
+            errors: errors
+        };
+    }
+
+
+    static computeStats(data, databaseId_to_team) {
         let statistics = {};
         let errors = {};
 
@@ -47,44 +92,44 @@ class IssueTimelineItems extends GithubGraphqlQuery {
             let issues = data.data.repository.issues;
             let issueNodes = issues.nodes;
             let timelineItemsTotalCountVector =
-               issueNodes.map( (n) => n.timelineItems.totalCount);
-            let sum = (a,b)=>a+b;
-            let timelineItemsCount = 
+                issueNodes.map((n) => n.timelineItems.totalCount);
+            let sum = (a, b) => a + b;
+            let timelineItemsCount =
                 timelineItemsTotalCountVector.reduce(sum, 0)
 
 
-            let issueTimelineItemsLoginsVector =  issueNodes.map( (n) => 
-                n.timelineItems.nodes.map( (timelineItem) =>{
-                    try{
-                        return(timelineItem.actor.login)
-                        
-                    }
-                    catch(e){
-                        console.log(`\nUNKNOWN EVENT TYPE= ${timelineItem.__typename}\n`)
-                        return("NOT AVAILABLE")
-                    }
-                    
-                }) 
-            ).flat();
-            let issueTimelineItemsDatabaseIdsVector =  issueNodes.map( (n) => 
-                n.timelineItems.nodes.map( (timelineItem) =>{
-                    try{
-                        return(timelineItem.actor.databaseId)
-                        
-                    }
-                    catch(e){
-                        return("NOT AVAILABLE")
-                    }
-                    
-                }) 
-            ).flat(); 
+            let issueTimelineItemsLoginsVector = issueNodes.map((n) =>
+                n.timelineItems.nodes.map((timelineItem) => {
+                    try {
+                        return (timelineItem.actor.login)
 
-            let timelineItemsCounts = 
+                    }
+                    catch (e) {
+                        console.log(`\nUNKNOWN EVENT TYPE= ${timelineItem.__typename}\n`)
+                        return ("NOT AVAILABLE")
+                    }
+
+                })
+            ).flat();
+            let issueTimelineItemsDatabaseIdsVector = issueNodes.map((n) =>
+                n.timelineItems.nodes.map((timelineItem) => {
+                    try {
+                        return (timelineItem.actor.databaseId)
+
+                    }
+                    catch (e) {
+                        return ("NOT AVAILABLE")
+                    }
+
+                })
+            ).flat();
+
+            let timelineItemsCounts =
                 vectorToCounts(issueTimelineItemsDatabaseIdsVector);
 
             let issueTimelineItemsTeamsVector = issueTimelineItemsDatabaseIdsVector.map(
                 (databaseId) => databaseId_to_team[databaseId]
-            ); 
+            );
             let timelineItemsTeamsCount = vectorToCounts(issueTimelineItemsTeamsVector);
             let issueTimelineItemsActorsCounts = vectorToCounts(issueTimelineItemsLoginsVector);
             statistics["totalIssues"] = issues.totalCount;
@@ -92,19 +137,18 @@ class IssueTimelineItems extends GithubGraphqlQuery {
             statistics["timelineItemsCounts"] = timelineItemsCounts;
             statistics["timelineItemsTeamsCount"] = timelineItemsTeamsCount
             statistics["issueTimelineItemsActorsCounts"] = issueTimelineItemsActorsCounts;
-            
-        } catch(e) { 
-             errors = {
-                 name : e.name,
-                 message: e.message
-             };
-         }
+
+        } catch (e) {
+            errors = {
+                name: e.name,
+                message: e.message
+            };
+        }
 
         return {
             statistics: statistics,
             errors: errors
         };
-        return {}
     }
 }
 
